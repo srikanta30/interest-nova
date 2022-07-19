d1e8f64e907eaf38173be5d8d790975919d4f4bd
srikanta-hexahealth <srikanta.banerjee@hexahealth.com>
2022-07-19 22:50:35 +0530

    Client Updates

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,4 +1,4 @@
-import { Layout, Input, message } from 'antd';
+import { Layout, Input, message, Card} from 'antd';
 import styles from './layout.module.css';
 import Results from '../Results';
 import React, { useState, useEffect } from 'react';
@@ -61,12 +61,15 @@ const LayoutComponent = () => {
         <Header>
           <h1 className={styles.menu_header}>Interest Nova 🧭</h1>
         </Header>
-        <Content style={{ padding: '0 50px' }}>
+        <Card style={{width: "100%"}}>
+        <Content>
           <div className="site-layout-content">
             <Input.Search placeholder="Search a broad term or keyword..." onSearch={(e) => setQuery(e)} size="large" style={{ marginTop: "5%" }} allowClear enterButton="Search" />
             <Results tableData={dataSource} loading={loading} />
           </div>
         </Content>
+        <div style={{marginTop: "1%", background: 'yellow', padding:".5%"}}><a href="https://www.paypal.me/srikanta30" target="_blank" rel="noreferrer">Found This Useful? Help Me Build More Awesome Tools 🚀</a></div>
+        </Card>
         <Footer style={{ textAlign: 'center' }}>Interest Nova 🧭 ©{new Date().getFullYear()} Created by <a href="https://in.linkedin.com/in/srikanta30" target="_blank" rel="noreferrer">Srikanta Banerjee</a> | <a href="https://docs.google.com/document/d/e/2PACX-1vTXEkU6pKM__pmvXBzbXoPU9zBknBLf3koqIeE1A7PKFbh9M2qav-G3uDuxBoNfm8bUQaksXcbGKbmc/pub" target="_blank" rel="noreferrer">Privacy Policy</a> </Footer>
       </Layout>
     </div>
